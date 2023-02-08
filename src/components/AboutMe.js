2b2d44e9658f50c33f1805c39b77f04318623aa7
darionjoness <101945727+darionjoness@users.noreply.github.com>
2023-02-08 14:08:57 -0600

    changed about section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import aboutImg from './img/aboutImg.jpeg'
+import bellah from './img/bellah.png'
 
 const AboutMe = ({ activeTab }) => {
   return (
     <div className={activeTab === 1 ? 'aboutMe container active' : 'notActive'}>
         <div className="aboutImg">
-            <img src={aboutImg} alt="" />
+            <img className='meImg' src={aboutImg} alt="" />
             <div className="bubble bubble-bottom-left">Hello 👋🏼, my name is Darion Jones. Nice to meet you!</div>
+            <img className='bellah' src={bellah} alt="" />
         </div>
         <div className="aboutText">
             <h1>About Me</h1>
-            <p>Hi, my name is Darion and I'm a 21 year old self taught Frontend Developer from Stillwater Oklahoma! I love to code, I have been coding for about a year now and love it more and more everyday! In my free time when I'm not coding, I like to invest in crypto, watch UFC fights, play basketball, play with my dog and hangout with my family! I am hardworking and love to learn!</p>
+            <p>Hi, my name is Darion and I'm a 21 year old self taught Frontend Developer from Stillwater Oklahoma! I love to code, I have been coding for over a year now and love it more and more everyday! In my free time when I'm not coding, I like to invest in crypto, watch UFC fights, play basketball, play with my dog (her names bellah, she is on the couch above) and hangout with my family! I am hardworking and love to learn!</p>
         </div>
     </div>
   )
